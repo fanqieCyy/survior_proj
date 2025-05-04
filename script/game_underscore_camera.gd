2a9bce688518a1cf0b3e3ac582d76a1c77dad0af
fanqieCyy <110085617+fanqieCyy@users.noreply.github.com>
2025-05-05 06:24:23 +0800

    TileMap和摄像机
    tilemap瞎画了个图(主场景)
    加了个摄像机（场景文件夹里）

---
+++ script/game_underscore_camera.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+
+var Target_position=Vector2.ZERO#这个变量是指向(0,0)向量的意思么
+
+  
+func _ready():
+	make_current()
+	
+	
+	
+func _process(delta):
+	Aquire_target()
+	global_position=global_position.lerp(Target_position,1.0-exp(-delta*10))#指数函数让相机跟随丝滑些
+	
+	
+func Aquire_target():
+	var player_nodes= get_tree().get_nodes_in_group("player")	 #获取该分组的信息
+	if player_nodes.size()>0:#不是很懂这段代码，照抄的教程，大概是让摄像机位置和玩家位置同步
+		var player=player_nodes[0] as Node2D#获取分组的第一个元素，即“player”
+		Target_position=player.global_position 
+		
