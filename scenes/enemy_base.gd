df8f5707a95c9d7ccd981fdb6f91e92b7401a53a
Hong Shengkai <98501282+huaziqi@users.noreply.github.com>
2025-05-13 00:11:10 +0800

    写好了基本的敌人生成器
    现在遇到的问题是玩家会推不动敌人

--- scenes/enemy_base.gd
+++ scenes/enemy_base.gd
@@ -6,9 +6,9 @@ var MAX_SPEED := 80
 var ACCELRATION := 2000
 
 func _ready() -> void:
-	pass
+	position = player.position - Vector2(randi_range(150, 200), randi_range(150, 300))
 
 func _physics_process(delta: float) -> void:
-	direction = self.position.direction_to(player.position) 
+	direction = self.position.direction_to(player.position)
 	velocity = velocity.move_toward(MAX_SPEED * direction, delta * ACCELRATION)
 	move_and_slide()
--- script/enemy_generator.gd
+++ script/enemy_generator.gd
@@ -1,10 +1,45 @@
 extends Node
 
-const ENEMY = preload("res://scenes/enemy.tscn")
+var scene_list : Dictionary = {}
+var enemy_num_list : Dictionary = {}
 @onready var player: Player = $"../Player"
 
+var enemyInfo : Dictionary = {
+	"enemy" : {
+		"update_time": 3,
+		"max_update_num": 10,
+		"max_num": 50
+	}
+}
+
 func _ready() -> void:
-	var enemy = ENEMY.instantiate()
-	enemy.player = player
-	add_child(enemy)
-	enemy.position = player.position - Vector2(100, 100)
+	handle_enemy_info()
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+		
+
+func handle_enemy_info(): #处理一下敌人数据
+	for key in enemyInfo:
+		var timer = Timer.new()
+		add_child(timer)
+		timer.wait_time = enemyInfo[key]["update_time"]
+		timer.one_shot = false
+		timer.timeout.connect(on_time_out.bind(key))
+		timer.start()
+		
+		enemy_num_list[key] = 0
+		var scene = load("res://scenes/enemies/" + key + ".tscn") as PackedScene
+		if(scene):
+			scene_list[key] = scene
+		
+
+func on_time_out(timer_id : String):
+	timer_id = timer_id.to_lower()
+	if(enemy_num_list[timer_id] < enemyInfo[timer_id]["max_num"]):
+		for i in range(0, min(enemyInfo[timer_id]["max_num"] - enemy_num_list[timer_id], enemyInfo[timer_id]["max_update_num"]) + 1):
+			enemy_num_list[timer_id] += 1
+			var enemy = scene_list[timer_id].instantiate()
+			enemy.player = player
+			add_child(enemy)
